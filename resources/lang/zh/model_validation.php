dc2a9bf2f218e59c69e8a2675ec60be08954df62
Shen LiangXiang <kj415j45@gmail.com>
2018-02-16 08:14:32 +0800

    Update model_validation.php

--- resources/lang/zh/model_validation.php
+++ resources/lang/zh/model_validation.php
@@ -31,7 +31,7 @@ return [
 
     'beatmapset_discussion' => [
         'beatmap_missing' => '指定了时间戳但是谱面不存在。',
-        'beatmapset_no_hype' => "无法推荐谱面。",
+        'beatmapset_no_hype' => '无法推荐谱面。',
         'hype_requires_null_beatmap' => '只能在 常规（全难度） 中推荐。',
         'invalid_beatmap_id' => '指定的难度无效。',
         'invalid_beatmapset_id' => '指定的谱面无效。',
